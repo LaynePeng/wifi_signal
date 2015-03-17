cbdb8c4ed797a9387b236c342449fc16c2231736
LaynePeng <appamail@hotmail.com>
2015-03-17 16:54:18 +0800

    1. Add more CLI; 2. Add the cal distance method.
    Signed-off-by: LaynePeng <appamail@hotmail.com>

--- LocationApi.py
+++ LocationApi.py
@@ -1,6 +1,7 @@
+import math
 from WindowsWifi import *
 
-def getSignalfor(*ssids):
+def getSignalfor(ssids):
     ifaces = getWirelessInterfaces()
     for iface in ifaces:
         #scanWlan(iface)
@@ -10,7 +11,7 @@ def getSignalfor(*ssids):
         signals = []
         
         for network in networks:
-            if network.ssid not in ssids:
+            if not network.ssid in ssids:
                 continue
 
             best_streanth = -100
@@ -28,3 +29,57 @@ def getSignalfor(*ssids):
             signals.append(signal_value)
 
         return signals
+
+def calDistance(freqInMHz, unit_strength, path_loss_exponent, signals):
+    distances = []
+    for signal in signals:
+        print signal
+        for ssid in signal.keys():
+            network_rssi = signal[ssid]["network_rssi"]
+            #result = (unit_strength - (20 * math.log10(freqInMHz)) + math.fabs(network_rssi)) / 20.0
+            #meters = math.pow(10, result)
+            meters = __general_modle(freqInMHz, unit_strength, path_loss_exponent, network_rssi)
+            one_distance = {}
+            one_distance[ssid] = meters
+
+            distances.append(one_distance)
+
+    return distances
+
+
+def __general_modle(freqInMHz, unit_strength, path_loss_exponent, rssi):
+    return math.pow(10, (rssi - unit_strength)/((-10.0)*path_loss_exponent))
+
+def __itu_modle(freqInMHz, unit_strength, path_loss_exponent, rssi):
+    lost = rssi - unit_strength
+    print lost
+    power = (lost-45.6)/30
+    print power
+    return math.pow(10, power) 
+
+
+def listAllNetwork():
+    ifaces = getWirelessInterfaces()
+    for iface in ifaces:
+        print iface
+        networks = getWirelessAvailableNetworkList(iface)
+        bssList = getWirelessNetworkBssList(iface)
+        print ""
+        for network in networks:
+            if network.ssid == "":
+                continue
+
+            print network.ssid
+            print network.bss_type
+            print ("Signal Quality: " + str(network.signal_quality) + "%")
+
+            best_streanth = -10
+            for bss in bssList:
+                if network.ssid == bss.ssid and bss.rssi < best_streanth:
+                    best_streanth = bss.rssi
+
+                if best_streanth < -100:
+                    best_streanth = -100
+            print "Signal Streath: " + str(best_streanth)
+            print "-" * 20
+        print ""
--- examples/test_signals.py
+++ examples/test_signals.py
@@ -5,11 +5,37 @@ sys.path.append('../')
 
 from LocationApi import *
 
+
+def __show_help():
+    print "--------------------------"
+    print "python test_signal.py show_all: Show all SSIDs found"
+    print "python test_signal.py trace --rssi *<ssid>: trace the rssi of the given ssid"
+    print "python test_signal.py trace --distance *<ssid>: trace the distance of the given ssid"
+    print "python test_signal.py help: show this help guide"
+
+def __cal_distances(signals):
+    #dLink_transmit_power = 15
+    unit_strength = -63
+    freqInMHz  = 2462
+    path_loss_exponent = 2
+    return calDistance(freqInMHz, unit_strength, path_loss_exponent, signals)
+
 if __name__ == "__main__":
     args = sys.argv[1:]
-    if len(args) == 0 or (len(args) > 0 and "test" == args[0]): 
-        print (getSignalfor("EMC Guest Cafe", "Pivotal", "CORP-W1F1"))
-    elif "trace" == args[0] and len(args) > 1:
+    if len(args) == 0 or (len(args) > 0 and "help" == args[0]): 
+        __show_help()
+    elif "show_all" == args[0]:
+        listAllNetwork()
+    elif "trace" == args[0] and len(args) > 2:
         while True: 
             time.sleep(3)
-            print getSignalfor(args[1])
+            if "--rssi" == args[1]:
+                print getSignalfor(args[2:])
+            elif "--distance" == args[1]:
+                siganls = getSignalfor(args[2:])
+                print __cal_distances(siganls)
+            else:
+                __show_help()
+                break
+    else:
+        __show_help()
